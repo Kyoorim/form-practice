02be746b87ec47bae5574bdff464341834ee6490
Kyoorim <kyoorim88@gmail.com>
2023-03-13 11:54:28 +0900

    타이핑할때 에러메세지 동시에 띄우기 작업 중

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -3,11 +3,15 @@ import { InputProps } from "../types/InputProps";
 import useInput from "../hooks/useInput";
 
 export const min = (length: number) => (value: string | number) => {
-  return String(value).length >= length;
+  if (String(value).length < length)
+    return `최소 ${length}글자 이상 입력해주세요`;
+  else undefined;
 };
 
 export const max = (length: number) => (value: string | number) => {
-  return String(value).length <= length;
+  if (String(value).length > length)
+    return `최대 ${length}글자 이하로 입력해주세요`;
+  else return undefined;
 };
 
 const TextField: FunctionComponent<InputProps> = ({
@@ -18,6 +22,11 @@ const TextField: FunctionComponent<InputProps> = ({
   validate,
 }) => {
   const { value, onChange, error } = useInput({ source, validate });
+
+  const errorMessage = {
+    name: `${validate[0](value) || ""}${validate[1](value) || ""}`,
+    password: `${validate[0](value) || ""}${validate[1](value) || ""}`,
+  };
 
   console.log(error);
   return (
@@ -31,6 +40,7 @@ const TextField: FunctionComponent<InputProps> = ({
           type={type}
           placeholder={placeholder}
         />
+        {error && <span style={{ color: "red" }}>{errorMessage[source]}</span>}
       </div>
     </div>
   );
--- src/containers/UserInfoForm.tsx
+++ src/containers/UserInfoForm.tsx
@@ -4,9 +4,9 @@ import TextField, { min, max } from "../components/TextField";
 function UserInfoForm(): JSX.Element {
   return (
     <SimpleForm>
-      <TextField validate={[min(3), max(3)]} source={"name"} label={"이름"} />
+      <TextField validate={[min(3), max(5)]} source={"name"} label={"이름"} />
       <TextField
-        validate={[min(3), max(3)]}
+        validate={[min(6), max(10)]}
         type="password"
         source={"password"}
         label={"비밀번호"}
